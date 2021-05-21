4dfd304c37f126f0ce204cec79bfe7db5b168416
Sunny Kathuria <41035660+sunny-kathuria@users.noreply.github.com>
2021-05-22 00:27:33 +0530

    Create index.js
    Quiz App

---
+++ index.js
@@ -0,0 +1,23 @@
+var input = require("readline-sync");
+var score=0;
+var user=input.question("What is your name? ");
+console.log("Welcome "+user+"!!!, Now start playing this Awesome Quiz. We hope that you will like it(Score based Quiz, It also have -ve marking)");
+
+function Quiz(Ques,Ans){
+  var userAns=input.question(Ques);
+    if(userAns===Ans){
+    score+=1;
+    console.log("Lovely!, your answer is correct.");
+  }
+  else{
+    score-=1;
+    console.log("Sad! Wrong answer, try again later");
+  }
+}
+
+
+Quiz("Who sent you this quiz(First name only)? ","Sunny");
+Quiz("Where does he live?(State only) ", "Haryana");
+Quiz("In which university, he studies? ","DIT University");
+console.clear()
+console.log("Thank You, "+user+"\nYour Score: "+score);
