700bec7e0b14cf77373d6f33e1c25e729431fe9b
Sunny Kathuria <41035660+sunny-kathuria@users.noreply.github.com>
2021-09-22 18:23:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,5 +19,7 @@ function Quiz(Ques,Ans){
 Quiz("Who sent you this quiz(First name only)? ","Sunny");
 Quiz("Where does he live?(State only) ", "Haryana");
 Quiz("In which university, he studies? ","DIT University");
+Quiz("When was he born? (year only)","1999");
+Quiz("In which company he is working?","IBM");
 console.clear()
 console.log("Thank You, "+user+"\nYour Score: "+score);
